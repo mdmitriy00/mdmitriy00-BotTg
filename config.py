2112509071c7f96c489d01f63ffbf995334479b8
mdmitriy00 <dimka12375@mail.ru>
2024-07-18 14:11:46 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+
+TOKEN = "7228110858:AAEutV4maMgYUtKzzb8XkhNZMjTg1DcyCS4"
+
+
+keys = {'рубль': 'RUB', 'доллар': 'USD', 'евро': 'EUR', 'юань': 'CNY'}
---
+++ extensions.py
@@ -0,0 +1,36 @@
+import requests
+import json
+from config import keys
+
+
+class ConversionExeption(Exception):
+    pass
+
+
+class ValueConverter():
+    @staticmethod
+    # def convert(self, message: telebot.types.Message):
+    def convert(quote: str, base: str, amount: str):
+
+        if quote == base:
+            raise ConversionExeption(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConversionExeption(f'Не удалось обработать валюту "{quote}".')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConversionExeption(f'Не удалось обработать валюту "{base}".')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConversionExeption(f'Не удалось обработать количество "{amount}".')
+
+        r = requests.get(f'https://v6.exchangerate-api.com/v6/30e0af3c5b97c935e6e51347/pair/{quote_ticker}/{base_ticker}')
+        total_base = json.loads(r.content)['conversion_rate']
+
+        return total_base
